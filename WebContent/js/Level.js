459b1f4899ce4c8c514a6e66c5db58b59effd099
Titaya <33252659+Titaya@users.noreply.github.com>
2017-12-10 21:43:08 +0700

    Update Level.js

--- WebContent/js/Level.js
+++ WebContent/js/Level.js
@@ -206,6 +206,8 @@ Level.prototype.addLost = function(){
 	return Lost;
 };	
 Level.prototype.Timeout = function(){
+	this.music.stop();
+	this.music3.play();
 	this.update(this.Lost.visible=true);
 	
 };	
